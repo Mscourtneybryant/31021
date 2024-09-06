31f27a22d8927f77228488641fceb24d063d3895
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-09-06 13:36:32 -0500

    complete

--- src/main/java/com/perscholas/springsoapdemo1/Springsoapdemo1Application.java
+++ src/main/java/com/perscholas/springsoapdemo1/Springsoapdemo1Application.java
@@ -3,6 +3,17 @@ package com.perscholas.springsoapdemo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+package com.perscholasMicroservice.EurekaServerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableEurekaServer
+
 @SpringBootApplication
 public class Springsoapdemo1Application {
 
