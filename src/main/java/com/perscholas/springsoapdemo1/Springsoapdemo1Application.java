204cc0b7dd0541089edffa08e02a1b91123cde3c
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-09-06 11:47:21 -0500

    start task

---
+++ src/main/java/com/perscholas/springsoapdemo1/Springsoapdemo1Application.java
@@ -0,0 +1,13 @@
+package com.perscholas.springsoapdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsoapdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsoapdemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/perscholas/springsoapdemo1/Springsoapdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.springsoapdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsoapdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
